676158cdfe6f4b3cba2de1e31f61d3531eb14df1
Ludmila Kostova <ludmila.kostova@yahoo.fr>
2021-08-26 13:10:30 +1000

    Added new exercises

---
+++ 3.Multidemensional lists_Matrix/2E. 2x2_square in matrix.py
@@ -0,0 +1,12 @@
+rows, cols = [int(el) for el in input().split(" ")]
+matrix = []
+
+for row in range(rows):
+    matrix.append([el for el in input().split(" ")])
+count = 0
+for row in range(rows - 1):
+    for col in range(cols - 1):
+        if matrix[row][col] == matrix[row][col + 1] == matrix[row + 1][col] == matrix[row + 1][col + 1]:
+            count += 1
+
+print(count)
